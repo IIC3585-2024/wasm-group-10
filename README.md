2679c5ef1bf2b9d9bcc7f58278b07ad5779d4be3
FGarciaVarela <pancho.garcia@uc.cl>
2024-04-13 18:54:36 -0400

    función en .c lista. Makefile básico listo

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # wasm-group-10
 
 `emcc lib/primeFactors.c -o func/primeFactors.js -sEXPORTED_FUNCTIONS=_primeFactors -sEXPORTED_RUNTIME_METHODS=ccall -s WASM=1`
+
+
+# Correr con makefile
+
+`make`
+`make clean`
+
+En caso de mensaje `bash: emcc: command not found`, correr lo siguiente pues significa que Emscripten no está activado en la terminal actual
+1) cd emsdk
+2) source ./emsdk_env.sh
+3) emcc --version
+
+
+Con esto ya debería funcionar nuevamente make y make clean.
+
--- lib/primeFactors.c
+++ lib/primeFactors.c
@@ -1,35 +1,56 @@
-// C Program to print all prime factors
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// A function to print all prime factors of a given number n
-void primeFactors(int n)
-{
-	// Print the number of 2s that divide n
-	while (n%2 == 0)
-	{
-		printf("%d ", 2);
-		n = n/2;
-	}
 
-	// n must be odd at this point. So we can skip 
-	// one element (Note i = i +2)
-	for (int i = 3; i*i <= n; i = i+2)
-	{
-		// While i divides n, print i and divide n
-		while (n%i == 0)
-		{
-			printf("%d ", i);
-			n = n/i;
-		}
-	}
+// Hecho con ayuda de ChatGPT. Algoritmo base de geeks of geeks "https://www.geeksforgeeks.org/c-program-for-efficiently-print-all-prime-factors-of-a-given-number/"
+// No funcionaba por tema de tamaño de memoria (números muy grandes no los manejaba), por lo que se usó esta solución;
+unsigned long long* primeFactors(const char* n_str, int* size) {
+    unsigned long long n = strtoull(n_str, NULL, 10); //convierte una cadena (array de caracteres, char*) a un número de tipo unsigned long long hasta encontrar NULL. Usa base 10
 
-	// This condition is to handle the case when n 
-	// is a prime number greater than 2
-	if (n > 2)
-		printf ("%d ", n);
+    printf("Starting prime factorization of %llu\n", n);
+    int arraySize = 5;
+    unsigned long long* factors = malloc(arraySize * sizeof(unsigned long long));
+    int count = 0; 
 
-    // Always flush at the end, if not for this line the output will not be printed
+
+    while (n % 2 == 0) {
+        if (count >= arraySize){
+            arraySize += 1;
+            factors = realloc(factors, arraySize * sizeof(unsigned long long));
+        }
+        factors[count++] = 2; // si es divisible por 2, el factor será 2
+        n /= 2;
+    }
+
+    for (unsigned long long i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            if (count >= arraySize){
+            arraySize += 1;
+            factors = realloc(factors, arraySize * sizeof(unsigned long long));
+            }
+            factors[count++] = i; // cada vez que se encuentra un factor primo se guarda
+            n /= i;
+        }
+    }
+
+    // Después de dividir n por todos sus posibles factores desde 2 hasta la raíz cuadrada de n, si n todavía es mayor que 2, entonces n mismo es un factor primo.
+    if (n > 2) {
+        if (count >= arraySize){
+            arraySize += 1;
+            factors = realloc(factors, arraySize * sizeof(unsigned long long));
+        }
+        factors[count++] = n;
+        
+    }
+    printf("Prime factors are: ");
+    for (int i = 0; i < count; i++) {
+        printf("%llu ", factors[i]);
+    }
     printf("\n");
+    *size = count;
+    return factors;
 }
 
-// This is code is improved by Susobhan Akhuli
+void freeFactors(unsigned long long* factors) {
+    free(factors);
+}
