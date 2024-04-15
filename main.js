c3e39115d0522ad3cd26df59ab3984d1184d4c2d
Cristóbal Berríos <crisberrios@uc.cl>
2024-04-15 11:21:03 -0400

    multiple wasms

--- main.js
+++ main.js
@@ -29,12 +29,16 @@ button?.addEventListener("click", () => {
   console.log(typeof runs);
 
   const resultJS = jsPrimeFactors(number, runs);
-  const resultWASM = wasmPrimeFactors(wasm, number, runs);
+  const resultWasm = wasmPrimeFactors(wasm, number, runs);
+  const resultWasmO1 = wasmPrimeFactors(wasmO1, number, runs);
+  const resultWasmO2 = wasmPrimeFactors(wasmO2, number, runs);
+  const resultWasmO3 = wasmPrimeFactors(wasmO3, number, runs);
 
-  displayFactors(resultJS.factors, resultJS.avgTime, resultWASM.factors, resultWASM.avgTime);
+  displayFactors(resultJS, resultWasm, resultWasmO1, resultWasmO2, resultWasmO3);
 });
 
 function jsPrimeFactors(number, runs) {
+  console.log("JS Prime Factors");
   let factors;
   let computeTime = 0;
 
@@ -51,6 +55,7 @@ function jsPrimeFactors(number, runs) {
 }
 
 function wasmPrimeFactors(wasmInstance, number, runs) {
+  console.log("WASM Prime Factors");
   // Module.cwrap es una función Emscripten para conectar c/c++ compilados en WebAssembly con js. Mas eficiente que ccall si se quiere haer varios llamados
   let primeFactors = wasmInstance.cwrap("primeFactors", "number", ["string", "number"]); //nombre de la función, tipo, tipos de los parámetros
   let computeTime = 0;
@@ -81,10 +86,21 @@ function wasmPrimeFactors(wasmInstance, number, runs) {
   return { factors, avgTime };
 }
 
-function displayFactors(factorsJS, avgJS, factorsWASM, avgWASM) {
+function displayFactors(resultJS, resultWasm, resultWasmO1, resultWasmO2, resultWasmO3) {
   const resultContainer = document.getElementById("result-container");
   resultContainer.innerHTML = `
-    <p>JavaScript Prime Factors: [${factorsJS.join(", ")}] - Computation Time: ${avgJS} ms</p>
-    <p>WebAssembly Prime Factors: [${Array.from(factorsWASM).join(", ")}] - Computation Time: ${avgWASM} ms</p>
+    <p>JavaScript Prime Factors: [${resultJS.factors.join(", ")}] - Computation Time: ${resultJS.avgTime} ms</p>
+    <p>WASM Default Prime Factors: [${Array.from(resultWasm.factors).join(", ")}] - Computation Time: ${
+    resultWasm.avgTime
+  } ms</p>
+  <p>WASM O1 Prime Factors: [${Array.from(resultWasmO1.factors).join(", ")}] - Computation Time: ${
+    resultWasmO1.avgTime
+  } ms</p>
+  <p>WASM O2 Prime Factors: [${Array.from(resultWasmO2.factors).join(", ")}] - Computation Time: ${
+    resultWasmO2.avgTime
+  } ms</p>
+  <p>WASM O3 Prime Factors: [${Array.from(resultWasmO3.factors).join(", ")}] - Computation Time: ${
+    resultWasmO3.avgTime
+  } ms</p>
   `;
 }
